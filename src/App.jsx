3fb3a99007f0208026e34bb02b5bcc455ce1d8b7
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-11 09:29:52 +0330

    نوشتن بخش ظاهری کارت ها

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import usePaginatedFetch from "./usePaginatedFetch";
-
+import { useEffect } from "react";
+import "../public/theme.min.css";
+import Card from "./components/card";
 const url =
   "https://react-mini-projects-api.classbon.com/Programmer/programmers";
 function App() {
-  const [loading, programmers] = usePaginatedFetch(url, 3);
-  return <div className="container"></div>;
+  const [loading, data] = usePaginatedFetch(url, 3);
+  const [page, setPage] = useState(1);
+  const [programmers, setProgrammers] = useState([]);
+  useEffect(() => {
+    if (loading) return;
+    setProgrammers(data[page - 1]);
+  }, [loading]);
+  return (
+    <div className="container pt-5">
+      {loading && (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border"></div>
+        </div>
+      )}
+      {!loading && (
+        <div className="row d-flex justify-content-center">
+          {programmers.map(({ id, ...programmer }) => {
+            return (
+              <div className="col-3" key={id}>
+                <Card {...programmer} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
